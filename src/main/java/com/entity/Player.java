6e98ffa3c3e545b45f12f6ece395cd6fc6587dff
lightning <junliang.lai.mmc@gmail.com>
2019-01-29 10:30:04 +0800

    继续修改

---
+++ src/main/java/com/entity/Player.java
@@ -0,0 +1,28 @@
+package com.entity;
+
+public class Player {
+    /**
+     * 用户ID
+     */
+	public String uid;
+	
+	/**
+     * 是否准备充分
+     */
+	public int ready=0;
+	
+	/**
+	 * 用户昵称
+	 */
+	public String nickName;
+	
+	/**
+	 * 用户头像
+	 */
+	public String avatar;
+	
+	/**
+	 * 用户金钱
+	 */
+	public float amount = 0f;
+}
--- src/main/java/com/entity/Room.java
+++ src/main/java/com/entity/Room.java
@@ -1,12 +1,15 @@
 package com.entity;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Room {
 	//房间ID
 	public int id;
+	//初始化手牌
+	public Brands brands;
 	//标记用户ID
-    public String[] player;
+	public Map<String, Player> map;
     //标记用户当前的money,初始化时，每个用户都需要上交
     public int[] money;
     //初始化用户数量
@@ -16,7 +19,10 @@ public class Room {
     //各项配置
     public HashMap<String, String> config;
     //给房价追加用户
-    public Room(int id) {
+    public Room(int id, int pnumber) {
     	this.id = id;
+    	brands = new Brands();
+    	map = new HashMap<String, Player>();
+    	number = pnumber;
     }
 }
